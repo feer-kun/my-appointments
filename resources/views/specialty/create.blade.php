56ff65bd3178005cf7cf95d6e2638aa508c73f03
ferkun <tooyuki777@gmail.com>
2022-09-22 16:45:31 -0500

    prueba2

--- resources/views/specialty/create.blade.php
+++ resources/views/specialty/create.blade.php
@@ -6,12 +6,12 @@
     <div class="col-12">
         <div class="card mb-4">
             <div class="card-header pb-0 d-flex justify-content-between">
-                <h6>Gestor de Especialidades</h6>
-                <a href="{{ route('specialties.create') }}"><button class="btn btn-primary" type="button">Nueva Especialidad</button></a>
+                <h6>Ingresa los datos de la nueva Especialidad</h6>
+                <a href="{{ route('specialties.create') }}"><button class="btn btn-primary" type="button"><i class="ni ni-fat-add"></i>Nueva Especialidad</button></a>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
                 <div class="table-responsive p-0">
-                   hola mundo 
+                    
                 </div>
             </div>
         </div>
